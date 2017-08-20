d460dff5b775c85de26c007ed7e9fc30aee9c10a
Chris Tosspon <chris.tosspon@objectpartners.com>
2017-08-20 16:20:16 -0500

    Adding logging configurations.

--- src/main/kotlin/com/twospoons/camel/kotlin/controller/SimpleController.kt
+++ src/main/kotlin/com/twospoons/camel/kotlin/controller/SimpleController.kt
@@ -1,13 +1,20 @@
 package com.twospoons.camel.kotlin.controller
 
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SimpleController {
 
+    companion object: KLogging()
+
     @GetMapping("/simple")
-    fun simple() =
-            "Hello, World!"
+    fun simple(): String {
+        logger.debug { "invoking /simple endpoint" }
+
+        return "Hello, World!"
+    }
+
 
 }
